ef88096619ad7d2be640f34506947b59e1a1545b
mrdoge2313 <101653082+mrdoge2313@users.noreply.github.com>
2022-07-05 01:10:20 +0200

    Update Sonicc.exee.lua

--- Sonicc.exee.lua
+++ Sonicc.exee.lua
@@ -1,67 +1,140 @@
+-- Gui to Lua
+-- Version: 3.2
 
+-- Instances:
 
 local ScreenGui = Instance.new("ScreenGui")
-local main = Instance.new("Frame")
-local label = Instance.new("TextLabel")
-local breaker = Instance.new("TextButton")
-local nullware = Instance.new("TextButton")
-local interxais = Instance.new("TextButton")
+local sonic = Instance.new("Frame")
+local TextLabel = Instance.new("TextLabel")
+local Tab1 = Instance.new("Frame")
+local TextLabel_2 = Instance.new("TextLabel")
+local TabHolder = Instance.new("Frame")
+local Tab1_2 = Instance.new("TextButton")
+local Tab2 = Instance.new("TextButton")
+local Tab2_2 = Instance.new("Frame")
+local TextButton = Instance.new("TextButton")
+local TextButton_2 = Instance.new("TextButton")
 
 --Properties:
 
 ScreenGui.Parent = game.CoreGui
 
-main.Name = "main"
-main.Parent = ScreenGui
-main.BackgroundColor3 = Color3.fromRGB(211, 255, 190)
-main.Position = UDim2.new(0.2265625, 0, 0.305315197, 0)
-main.Size = UDim2.new(0, 635, 0, 297)
-main.Active = true
-main.Draggable = true
+sonic.Name = "sonic"
+sonic.Parent = ScreenGui
+sonic.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+sonic.Position = UDim2.new(0.618181825, 0, 0.668726802, 0)
+sonic.Size = UDim2.new(0, 544, 0, 262)
 
-label.Name = "label"
-label.Parent = main
-label.BackgroundColor3 = Color3.fromRGB(43, 89, 255)
-label.Size = UDim2.new(0, 635, 0, 50)
-label.Font = Enum.Font.SourceSans
-label.Text = "sonicc v1"
-label.TextColor3 = Color3.fromRGB(0, 0, 0)
-label.TextSize = 14.000
+TextLabel.Parent = sonic
+TextLabel.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.BackgroundTransparency = 100.000
+TextLabel.BorderColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel.Position = UDim2.new(-0.102570839, 0, -0.034351144, 0)
+TextLabel.Size = UDim2.new(0, 190, 0, 42)
+TextLabel.Font = Enum.Font.GothamBold
+TextLabel.Text = "Sonic.exe"
+TextLabel.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel.TextSize = 18.000
 
-breaker.Name = "breaker"
-breaker.Parent = main
-breaker.BackgroundColor3 = Color3.fromRGB(255, 17, 17)
-breaker.Position = UDim2.new(0, 0, 0.168350175, 0)
-breaker.Size = UDim2.new(0, 313, 0, 118)
-breaker.Font = Enum.Font.SourceSans
-breaker.Text = "breaker"
-breaker.TextColor3 = Color3.fromRGB(0, 0, 0)
-breaker.TextSize = 24.000
-breaker.MouseButton1Down:connect(function()
-	loadstring(game:HttpGet(("https://raw.githubusercontent.com/OmarXploits/Breaker.exe/main/Breaker.exe.lua"),true))()
-end)
+Tab1.Name = "Tab1"
+Tab1.Parent = sonic
+Tab1.BackgroundColor3 = Color3.fromRGB(106, 106, 106)
+Tab1.Position = UDim2.new(0.219780222, 0, 0.0534351133, 0)
+Tab1.Size = UDim2.new(0, 426, 0, 234)
+Tab1.Visible = false
 
-nullware.Name = "nullware"
-nullware.Parent = main
-nullware.BackgroundColor3 = Color3.fromRGB(97, 234, 255)
-nullware.Position = UDim2.new(0.492913395, 0, 0.168350175, 0)
-nullware.Size = UDim2.new(0, 322, 0, 118)
-nullware.Font = Enum.Font.SourceSans
-nullware.Text = "nullware"
-nullware.TextColor3 = Color3.fromRGB(0, 0, 0)
-nullware.TextSize = 24.000
-breaker.MouseButton1Down:connect(function()
-	getgenv().Theme = "Blue" -- To change the UI Theme, set this to one of the following options: "Red", "Purple", "Blue", "Green", "Yellow"
-	loadstring(game:HttpGet("https://gist.githubusercontent.com/M6HqVBcddw2qaN4s/2d722888a388017c18028cd434c43a25/raw/dcccf1027fe4b90780e47767aaf584389c9d7771/EULma3fU90PUOKUn?identifier=".. (function()local a=""for b=1,256 do local c=math.random(1,3)a=a..string.char(c==1 and math.random(48,57)or c==2 and math.random(97,122)or c==3 and math.random(65,90))end;return a end)()))()
-end)
+TextLabel_2.Parent = Tab1
+TextLabel_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TextLabel_2.BackgroundTransparency = 100.000
+TextLabel_2.Position = UDim2.new(0.237089202, 0, 0.277777791, 0)
+TextLabel_2.Size = UDim2.new(0, 200, 0, 50)
+TextLabel_2.Font = Enum.Font.SciFi
+TextLabel_2.Text = "made by mr_chinese#7614"
+TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextLabel_2.TextSize = 41.000
 
+TabHolder.Name = "TabHolder"
+TabHolder.Parent = sonic
+TabHolder.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TabHolder.Position = UDim2.new(0.00183150184, 0, 0.339694649, 0)
+TabHolder.Size = UDim2.new(0, 100, 0, 38)
 
-interxais.Name = "interxais"
-interxais.Parent = main
-interxais.BackgroundColor3 = Color3.fromRGB(255, 161, 161)
-interxais.Position = UDim2.new(0, 0, 0.565656543, 0)
-interxais.Size = UDim2.new(0, 635, 0, 129)
-interxais.Font = Enum.Font.SourceSans
-interxais.Text = "interxais"
-interxais.TextColor3 = Color3.fromRGB(0, 0, 0)
-interxais.TextSize = 24.000
+Tab1_2.Name = "Tab1"
+Tab1_2.Parent = TabHolder
+Tab1_2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Tab1_2.Size = UDim2.new(0, 100, 0, 38)
+Tab1_2.Font = Enum.Font.SciFi
+Tab1_2.Text = "Credits"
+Tab1_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Tab1_2.TextSize = 24.000
+Tab1_2.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+
+Tab2.Name = "Tab2"
+Tab2.Parent = TabHolder
+Tab2.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
+Tab2.Position = UDim2.new(-0.00999999978, 0, -1.26315796, 0)
+Tab2.Size = UDim2.new(0, 100, 0, 38)
+Tab2.Font = Enum.Font.SciFi
+Tab2.Text = "Scripts"
+Tab2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Tab2.TextSize = 24.000
+Tab2.TextStrokeColor3 = Color3.fromRGB(255, 255, 255)
+
+Tab2_2.Name = "Tab2"
+Tab2_2.Parent = sonic
+Tab2_2.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Tab2_2.Position = UDim2.new(0.247252747, 0, 0, 0)
+Tab2_2.Size = UDim2.new(0, 411, 0, 262)
+
+TextButton.Parent = Tab2_2
+TextButton.BackgroundColor3 = Color3.fromRGB(52, 52, 52)
+TextButton.Size = UDim2.new(0, 411, 0, 262)
+TextButton.Font = Enum.Font.SourceSans
+TextButton.Text = "Soon"
+TextButton.TextColor3 = Color3.fromRGB(225, 225, 225)
+TextButton.TextSize = 82.000
+
+TextButton_2.Parent = ScreenGui
+TextButton_2.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
+TextButton_2.Position = UDim2.new(0, 0, 0.584672451, 0)
+TextButton_2.Size = UDim2.new(0, 163, 0, 50)
+TextButton_2.Font = Enum.Font.GothamBold
+TextButton_2.Text = "X"
+TextButton_2.TextColor3 = Color3.fromRGB(0, 0, 0)
+TextButton_2.TextSize = 19.000
+
+-- Scripts:
+
+local function BRSRAW_fake_script() -- Tab1_2.LocalScript 
+	local script = Instance.new('LocalScript', Tab1_2)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.Parent.Tab1.Visible = true
+		script.Parent.Parent.Parent.Tab2.Visible = false
+	end) 
+end
+coroutine.wrap(BRSRAW_fake_script)()
+local function NQFU_fake_script() -- Tab2.LocalScript 
+	local script = Instance.new('LocalScript', Tab2)
+
+	script.Parent.MouseButton1Down:Connect(function()
+		script.Parent.Parent.Parent.Tab1.Visible = false
+		script.Parent.Parent.Parent.Tab2.Visible = true
+	end) 
+end
+coroutine.wrap(NQFU_fake_script)()
+local function QWOKIMV_fake_script() -- TextButton_2.LocalScript 
+	local script = Instance.new('LocalScript', TextButton_2)
+
+	local frame = script.Parent.Parent.sonic
+	local open = false
+	script.Parent.MouseButton1Click:Connect(function()
+		if frame.Visible == false then 
+			frame.Visible = true 
+		
+		else
+			frame.Visible = false
+		end
+		end)
+end
+coroutine.wrap(QWOKIMV_fake_script)()
