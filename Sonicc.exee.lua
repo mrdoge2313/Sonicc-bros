e1736888eb099903e70a0ce580ffeb666ae87579
mrdoge2313 <101653082+mrdoge2313@users.noreply.github.com>
2022-07-05 13:38:49 +0200

    Update Sonicc.exee.lua

--- Sonicc.exee.lua
+++ Sonicc.exee.lua
@@ -49,9 +49,9 @@ TextLabel_2.BackgroundTransparency = 100.000
 TextLabel_2.Position = UDim2.new(0.237089202, 0, 0.277777791, 0)
 TextLabel_2.Size = UDim2.new(0, 200, 0, 50)
 TextLabel_2.Font = Enum.Font.SciFi
-TextLabel_2.Text = "Ui by mr_sad2579\\nScripter:\\nmr_sad2579"
+TextLabel_2.Text = "Made by mr_chinese#7614"
 TextLabel_2.TextColor3 = Color3.fromRGB(0, 0, 0)
-TextLabel_2.TextSize = 52.000
+TextLabel_2.TextSize = 41.000
 
 TabHolder.Name = "TabHolder"
 TabHolder.Parent = sonic
